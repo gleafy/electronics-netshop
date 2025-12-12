cbcb14effbc5235ea5b92a611f9283a232c734f9
gleafy <gleafy@cock.li>
2025-12-12 04:18:42 +0400

    create network views

--- network/views.py
+++ network/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, filters
 
-# Create your views here.
+from .models import NetworkNode, Product
+from .serializers import NetworkNodeSerializer, ProductSerializer
+from .permissions import IsActiveEmployee
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsActiveEmployee]
+
+
+class NetworkNodeViewSet(viewsets.ModelViewSet):
+    queryset = NetworkNode.objects.select_related("supplier").all()
+    serializer_class = NetworkNodeSerializer
+    permission_classes = [IsActiveEmployee]
+    
+    # Фильтрация и поиск
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ["country"]
+    search_fields = ["name", "city"]
