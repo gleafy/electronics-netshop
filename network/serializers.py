1e91d127d822f4aa25c5c16d749b7d5ee69aab81
gleafy <gleafy@cock.li>
2025-12-12 05:46:26 +0400

    fixed serializers.py

--- network/serializers.py
+++ network/serializers.py
@@ -19,15 +19,30 @@ class NetworkNodeSerializer(serializers.ModelSerializer):
         Проверка корректности выбора поставщика.
         """
         supplier = data.get("supplier")
-        
+
+        node_type = data.get(
+            "node_type", self.instance.node_type if self.instance else None
+        )
+
+        if node_type == NetworkNode.NodeType.FACTORY and supplier:
+            raise serializers.ValidationError(
+                {
+                    "supplier": "Завод всегда находится на уровне 0 и не может иметь поставщика."
+                }
+            )
+
         if self.instance:
             if supplier and supplier.id == self.instance.id:
-                raise serializers.ValidationError({"supplier": "Звено не может быть своим поставщиком."})
+                raise serializers.ValidationError(
+                    {"supplier": "Звено не может быть своим поставщиком."}
+                )
 
         if supplier:
             if supplier.level >= 2:
                 raise serializers.ValidationError(
-                    {"supplier": "Выбранный поставщик находится на низшем уровне иерархии и не может иметь подчиненных."}
+                    {
+                        "supplier": "Выбранный поставщик находится на низшем уровне иерархии и не может иметь подчиненных."
+                    }
                 )
 
         return data
